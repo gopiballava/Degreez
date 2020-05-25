3975ccb7a3ca27350e8338fa3136338ffb971e43
Ajay Merchia <ajaymerchia@berkeley.edu>
2020-05-24 19:57:45 -0700

    Change default refresh to 1 hour

--- Degreez/Shared/WeatherAPI.swift
+++ Degreez/Shared/WeatherAPI.swift
@@ -36,7 +36,7 @@ class WeatherAPI: NSObject, CLLocationManagerDelegate {
 	var currentLocation = CLLocation()
 	
 	var lastUpdate = Date(timeIntervalSince1970: 0)
-	let refreshThreshold: TimeInterval = 15 * TimeInterval.minute
+	let refreshThreshold: TimeInterval = 30 * TimeInterval.minute
 	
 	var kelvin: Kelvin = 233.15 {
 		didSet {
